5791e1d8322de3e5d73e66dbc3ed1d209d7032bb
Akash_Ktr <akash.katiyar.met21@itbhu.ac.in>
2023-01-23 19:58:07 +0530

    till Update Note

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
 const connectToMongo = require('./db');
 const express = require('express')
-connectToMongo();
 
+connectToMongo();
 const app = express()
 const port = 4000
 
 app.use(express.json())
 
 // Available Routes
-app.use('/api/auth', require('./routes/auth.js'));
-app.use('/api/auth', require('./routes/notes.js'));
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
